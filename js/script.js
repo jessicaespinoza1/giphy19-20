05283daf9eb94934fbc6749d17cd32a6b09f9936
jessicaespinoza1 <13jessica.espinoza@gmail.com>
2020-02-20 02:31:18 +0000

    worked on HTML and Javascript

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 /* global $ */
-
 console.log('hi');
 
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+$('button').click(function(){
+  fetch("https://api.giphy.com/v1/gifs/search?rating=pg&api_key=dc6zaTOxFJmzC&q=puppy")
+  .then(function(response){
+    return response.json();
+})
+    .then(function(data){
+      console.log(data);
+    });
 });
