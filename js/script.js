1274614b502324d9d4ca40e86bbea1f2970f2da7
jessicaespinoza1 <13jessica.espinoza@gmail.com>
2020-03-05 02:32:27 +0000

    took user input from the input box when updated API

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@
 console.log('hi');
 
 $('button').click(function(){
-  fetch("https://api.giphy.com/v1/gifs/search?rating=pg&api_key=dc6zaTOxFJmzC&q=puppy")
+  var input = $("input").val();
+  var name = "https://api.giphy.com/v1/gifs/search?rating=pg&api_key=dc6zaTOxFJmzC&q=" + input;
+  fetch(name)
   .then(function(response){
     return response.json();
 })
     .then(function(data){
       var Url = data.data[0].images.original.url;
       $("body").append(`<img src="${Url}">`);
+     
     });
 });
